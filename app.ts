651df7338148f9b008f4d510bb9c08ad1c8b2122
towa1204 <lsadsfj@gmail.com>
2022-08-22 23:31:30 +0900

    CLIオプションの導入

--- app.ts
+++ app.ts
@@ -1,10 +1,21 @@
 import puppeteer from 'puppeteer';
+import { Command } from 'commander';
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({ path: 'example.png' });
+const program = new Command();
+program
+  .requiredOption('-p, --project <projectname>', 'scrapbox projectname')
+  .option('-c, --connectsid <value>', 'connect.sid value (used for private projects)')
+  .parse(process.argv);
 
-  await browser.close();
-})();
+const options = program.opts();
+console.log(`projectname: ${options.project}`);
+console.log(`connect.sid: ${options.connectsid}`);
+
+// (async () => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.goto('https://example.com');
+//   await page.screenshot({ path: 'example.png' });
+
+//   await browser.close();
+// })();
