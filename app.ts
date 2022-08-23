6565ea98001a452d62150b9be8ac6b150c6525cf
towa1204 <lsadsfj@gmail.com>
2022-08-24 01:36:35 +0900

    ランダムにページを取得する処理の実装

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import puppeteer from 'puppeteer';
+import fetch from 'node-fetch';
 import { Command } from 'commander';
 
 const program = new Command();
@@ -10,6 +11,34 @@ program
 const options = program.opts();
 console.log(`projectname: ${options.project}`);
 console.log(`connect.sid: ${options.connectsid}`);
+
+const fetchOpts =
+  options.connectsid !== undefined
+    ? {
+        headers: {
+          cookie: `connect.sid=${options.connectsid}`,
+        },
+      }
+    : undefined;
+
+const getRandomPage = async (projectname: string) => {
+  // TODO: fetch のエラー処理
+  const pageSize: number = (
+    await (await fetch(`https://scrapbox.io/api/pages/${projectname}?limit=1`, fetchOpts)).json()
+  ).count; // プロジェクトの総ページ数
+  const randNum = Math.floor(Math.random() * pageSize); // 0 <= randNum < pageSize
+  const randPageTitle: string = (
+    await (await fetch(`https://scrapbox.io/api/pages/${projectname}?limit=1&skip=${randNum}`, fetchOpts)).json()
+  ).pages[0].title;
+
+  return { title: randPageTitle, url: `https://scrapbox.io/${projectname}/${encodeURIComponent(randPageTitle)}` };
+};
+const main = async () => {
+  const pageLink = await getRandomPage(options.project);
+  console.log(pageLink);
+};
+
+main();
 
 // (async () => {
 //   const browser = await puppeteer.launch();
